95fba0050a417edcfe0115f318b2f7e67d09856b
Billy Yang <mz.yyam@gmail.com>
2025-01-15 10:34:50 +0800

    fix: 演示页面转arco design

--- apps/web-arco/src/views/demos/naive/index.vue
+++ apps/web-arco/src/views/demos/naive/index.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { Page } from '@vben/common-ui';
 
-import { Message } from '@arco-design/web-vue';
+import { Button, Card, Message, Space } from '@arco-design/web-vue';
 
 function error() {
   Message.error('Once upon a time you dressed so fine');
@@ -37,34 +37,34 @@ function notify(type: 'error' | 'info' | 'success' | 'warning') {
     description="支持多语言，主题功能集成切换等"
     title="Arco Design组件使用演示"
   >
-    <a-card class="mb-5" title="按钮">
-      <a-space>
-        <a-button>Default</a-button>
-        <a-button type="secondary">Secondary</a-button>
-        <a-button type="primary">Primary</a-button>
-        <a-button type="outline">Info</a-button>
-        <a-button status="success">Success</a-button>
-        <a-button status="warning">Warning</a-button>
-        <a-button status="danger">Error</a-button>
-      </a-space>
-    </a-card>
+    <Card class="mb-5" title="按钮">
+      <Space>
+        <Button>Default</Button>
+        <Button type="secondary">Secondary</Button>
+        <Button type="primary">Primary</Button>
+        <Button type="outline">Info</Button>
+        <Button status="success">Success</Button>
+        <Button status="warning">Warning</Button>
+        <Button status="danger">Error</Button>
+      </Space>
+    </Card>
 
-    <a-card class="mb-5" title="Message">
-      <a-space>
-        <a-button status="danger" @click="error">错误</a-button>
-        <a-button status="warning" @click="warning">警告</a-button>
-        <a-button status="success" @click="success">成功</a-button>
-        <a-button type="primary" @click="loading">加载中</a-button>
-      </a-space>
-    </a-card>
+    <Card class="mb-5" title="Message">
+      <Space>
+        <Button status="danger" @click="error">错误</Button>
+        <Button status="warning" @click="warning">警告</Button>
+        <Button status="success" @click="success">成功</Button>
+        <Button type="primary" @click="loading">加载中</Button>
+      </Space>
+    </Card>
 
-    <a-card class="mb-5" title="Notification">
-      <a-space>
-        <a-button status="danger" @click="notify('error')">错误</a-button>
-        <a-button status="warning" @click="notify('warning')">警告</a-button>
-        <a-button status="success" @click="notify('success')">成功</a-button>
-        <a-button type="primary" @click="notify('info')">加载中</a-button>
-      </a-space>
-    </a-card>
+    <Card class="mb-5" title="Notification">
+      <Space>
+        <Button status="danger" @click="notify('error')">错误</Button>
+        <Button status="warning" @click="notify('warning')">警告</Button>
+        <Button status="success" @click="notify('success')">成功</Button>
+        <Button type="primary" @click="notify('info')">加载中</Button>
+      </Space>
+    </Card>
   </Page>
 </template>
