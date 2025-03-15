39b39dbdff46ac4ef79087dcc1a2c96d796a88d0
Billy Yang <mz.yyam@gmail.com>
2025-03-15 09:18:25 +0800

    feat: 系统设置内的菜单结构调整，把权限相关的全部转移到权限管理内

--- apps/web-arco/src/router/routes/modules/system.ts
+++ apps/web-arco/src/router/routes/modules/system.ts
@@ -12,49 +12,58 @@ const routes: RouteRecordRaw[] = [
     path: '/system',
     children: [
       {
-        name: 'SystemDept',
-        path: '/system/dept',
-        component: () => import('#/views/system/dept/list.vue'),
+        name: 'AccountPermission',
+        path: '/system/account/permission',
         meta: {
-          icon: 'lucide:network',
-          title: $t('system.dept.title'),
+          title: $t('system.account.permission.title'),
         },
-      },
-      {
-        path: '/system/menu',
-        name: 'SystemMenu',
-        meta: {
-          icon: 'mdi:menu',
-          title: $t('system.menu.title'),
-        },
-        component: () => import('#/views/system/menu/list.vue'),
-      },
-      {
-        path: '/system/role',
-        name: 'SystemRole',
-        meta: {
-          icon: 'mdi:account-group',
-          title: $t('system.role.title'),
-        },
-        component: () => import('#/views/system/role/list.vue'),
-      },
-      {
-        path: '/system/permission',
-        name: 'SystemRolePermission',
-        meta: {
-          icon: 'mdi:user-access-control',
-          title: $t('system.permission.title'),
-        },
-        component: () => import('#/views/system/permission/index.vue'),
-      },
-      {
-        path: '/system/account',
-        name: 'SystemAccount',
-        meta: {
-          icon: 'mdi:account-outline',
-          title: $t('system.account.title'),
-        },
-        component: () => import('#/views/system/account/index.vue'),
+        children: [
+          {
+            name: 'SystemDept',
+            path: '/system/dept',
+            component: () => import('#/views/system/dept/list.vue'),
+            meta: {
+              icon: 'lucide:network',
+              title: $t('system.dept.title'),
+            },
+          },
+          {
+            path: '/system/menu',
+            name: 'SystemMenu',
+            meta: {
+              icon: 'mdi:menu',
+              title: $t('system.menu.title'),
+            },
+            component: () => import('#/views/system/menu/list.vue'),
+          },
+          {
+            path: '/system/role',
+            name: 'SystemRole',
+            meta: {
+              icon: 'mdi:account-group',
+              title: $t('system.role.title'),
+            },
+            component: () => import('#/views/system/role/list.vue'),
+          },
+          {
+            path: '/system/permission',
+            name: 'SystemRolePermission',
+            meta: {
+              icon: 'mdi:user-access-control',
+              title: $t('system.permission.title'),
+            },
+            component: () => import('#/views/system/permission/index.vue'),
+          },
+          {
+            path: '/system/account',
+            name: 'SystemAccount',
+            meta: {
+              icon: 'mdi:account-outline',
+              title: $t('system.account.title'),
+            },
+            component: () => import('#/views/system/account/index.vue'),
+          },
+        ],
       },
     ],
   },
