3f96b24d418ee72d7ceffb2eee32084671a5e139
Billy Yang <mz.yyam@gmail.com>
2025-07-30 16:38:02 +0800

    fix: 修正默认主页路径为'/workspace'

--- apps/web-arco/src/preferences.ts
+++ apps/web-arco/src/preferences.ts
@@ -14,7 +14,7 @@ export const overridesPreferences = defineOverridesPreferences({
     name: import.meta.env.VITE_APP_TITLE,
     enableRefreshToken: false,
     accessMode: import.meta.env.VITE_APP_ACCESS_MODE,
-    defaultHomePath: '/workplace',
+    defaultHomePath: '/workspace',
   },
   footer: {
     enable: true,
